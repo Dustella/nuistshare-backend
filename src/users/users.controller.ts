61e824dc70ecbd29493c19ccf322a76315674968
Dustella <fdnoaivj@outlook.com>
2023-08-06 13:08:20 +0800

    🚑 fix big issue

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,31 +6,31 @@ import {
   Query,
   Req,
   UseGuards,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
-import { LoginForm, RegisterForm } from './dto/form.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthGuard } from 'src/auth/auth.guard';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { LoginForm, RegisterForm } from "./dto/form.dto";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller()
 export class UsersController {
   constructor(private prisma: PrismaService, private users: UsersService) {}
 
-  @Post('/api/login')
+  @Post("/api/login")
   async login(@Body() req: LoginForm) {
     const { email, password } = req;
     return this.users.login(email, password);
   }
 
-  @Post('/api/register')
+  @Post("/api/register")
   async register(@Body() req: RegisterForm) {
     const { email, password, username } = req;
     return this.users.register(email, password, username);
   }
 
   @UseGuards(AuthGuard)
-  @Get('/api/users/me')
-  async getMe(@Req() req) {
+  @Get("/api/users/me")
+  async getMe(@Req() req: any) {
     const { sub } = req.user;
     return await this.users.getUserRecord(sub);
   }
